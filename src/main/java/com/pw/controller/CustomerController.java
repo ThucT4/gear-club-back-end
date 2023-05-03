abbd19a43276b26739c7de46cfa68a370c8d76bd
Bacenz <bentheuchiha@gmail.com>
2023-05-03 13:43:07 +0700

    random stuff for controller

--- src/main/java/com/pw/controller/CustomerController.java
+++ src/main/java/com/pw/controller/CustomerController.java
@@ -1,4 +1,18 @@
 package com.pw.controller;
 
+import com.pw.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/product")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
 }
