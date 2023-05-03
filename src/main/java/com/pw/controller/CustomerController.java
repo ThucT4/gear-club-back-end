2b833d318d968fbaac4e8db42487f212455a882b
Bacenz <bentheuchiha@gmail.com>
2023-05-03 18:01:13 +0700

    basic customer controller

--- src/main/java/com/pw/controller/CustomerController.java
+++ src/main/java/com/pw/controller/CustomerController.java
@@ -1,18 +1,41 @@
 package com.pw.controller;
 
+import com.pw.model.Customer;
+import com.pw.model.Product;
 import com.pw.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/product")
+@RequestMapping("api/customer")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @PostMapping(value = "/", consumes = "application/json")
+    public Customer create(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Customer retrieve(@PathVariable int id) {
+        return customerService.retrieve(id);
+    }
+
+    @PutMapping(value = "/", consumes = "application/json")
+    public Customer update(@RequestBody Customer customer) {
+        return customerService.update(customer);
+    }
+
+    @DeleteMapping(value = "/")
+    public String delete(int id) {
+        customerService.delete(id);
+        return "Done";
+    }
+
+
 }
