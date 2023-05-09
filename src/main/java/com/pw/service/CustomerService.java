01827e26bffe29933563d23aaa6eda0c90940c34
Bacenz <bentheuchiha@gmail.com>
2023-05-10 00:03:11 +0700

    optimize import

--- src/main/java/com/pw/service/CustomerService.java
+++ src/main/java/com/pw/service/CustomerService.java
@@ -7,10 +7,7 @@ import com.pw.repository.CustomerRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 
 import java.time.Instant;
 import java.util.ArrayList;
